976df59a945d355cff9c48695dd5b7e9221a3ebc
EasyIce <120899174+IamEasyIce@users.noreply.github.com>
2023-01-24 13:33:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -242,7 +242,7 @@ async def next_page(bot, query):
                     ],
                 )
     btn.insert(0, [
-        InlineKeyboardButton("‼️ How To Download ⁉️", url=f"https://t.me/MrperfectOffcial/130")
+        InlineKeyboardButton("‼️ How To Download ⁉️", url=f"https://t.me/EasySky_Link")
     ])
     try:
         await query.edit_message_reply_markup(
@@ -1335,7 +1335,7 @@ async def auto_filter(client, msg, spoll=False):
             )
 
     btn.insert(0, [
-        InlineKeyboardButton("‼️ How To Download ⁉️", url=f"https://t.me/MrperfectOffcial/130")
+        InlineKeyboardButton("‼️ How To Download ⁉️", url=f"https://t.me/EasySky_Link")
     ])
 
     if offset != "":
