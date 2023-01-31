044e736051a3514eb0a89714d7e6f355c765309d
EasyIce <120899174+IamEasyIce@users.noreply.github.com>
2023-01-31 13:26:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PriyankaH
 fi
-cd /NataliyaH
+cd /PriyankaH
 pip3 install -U -r requirements.txt
 echo "Starting Priyanka...."
 python3 bot.py
